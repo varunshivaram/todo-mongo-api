bae7c698716a99d23422026d66eb901e1a5d3ced
varunshivaram <csvarun1995@gmail.com>
2019-02-14 13:05:13 +0530

    added the PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb')
+
+
 
 var app =express();
 var port = process.env.PORT || 3000;
@@ -67,6 +70,34 @@ app.delete('/todos/:id', (req, res) => {
       res.status(400).send();
     });
   });
+
+
+app.patch('/todos/:id' , (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body , [ 'text' , 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+      }
+    
+    if (_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id , {$set:body} , {new:true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.status(200).send({todo})
+    }).catch((e) => {
+        res.status(400).send();
+    });
+    
+
+});
 
 
 
